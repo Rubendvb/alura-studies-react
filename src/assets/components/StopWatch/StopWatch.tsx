76febccc0a3bb109dde345746da627ea884f0492
Rubendvb <rubenvasquez556@gmail.com>
2022-10-01 14:31:11 -0300

    Criando função para tratamento das horas em segundos

--- src/assets/components/StopWatch/StopWatch.tsx
+++ src/assets/components/StopWatch/StopWatch.tsx
@@ -3,7 +3,11 @@ import { Watch } from "./Watch/Watch";
 
 import style from "./StopWatch.module.scss";
 
+import { tempoParaSegundos } from "../../common/utils/date";
+
 export function StopWatch() {
+  console.log("Conversão: ", tempoParaSegundos("01:01:01"));
+
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Inicia cronometro</p>
